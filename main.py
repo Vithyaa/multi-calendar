c101654584009c196184f7a3b0cffc679ec1a9d2
john-jeffrey-J4 <123716538+john-jeffrey-J4@users.noreply.github.com>
2024-03-13 16:05:15 +0530

    changes

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String, 
 from sqlalchemy.sql import select
 from pydantic import BaseModel
 from models import calendar
+from datetime import datetime, timezone
 
 app = FastAPI()
 
@@ -43,87 +44,109 @@ class CalendarUpdate(BaseModel):
 # Create a new calendar
 
 
-@app.post("/calendars/", response_model=CalendarCreate)
-async def create_calendar(calendar: CalendarCreate):
+# @app.post("/calendars/", response_model=CalendarCreate)
+# async def create_calendar(calendar: CalendarCreate):
+#     try:
+#         query = calendars.insert().values(
+#             name=calendar.name,
+#             calendar_type=calendar.calendar_type,
+#             api_key=calendar.api_key,
+#             show_busy_only=calendar.show_busy_only,
+#             is_private=calendar.is_private,
+#         )
+#         last_record_id = await database.execute(query)
+#         return {**calendar.dict(), "id": last_record_id}
+#     except Exception as e:
+#         logging.exception("An error occurred while creating calendar: %s", e)
+#         raise
+
+# # Read a specific calendar by its ID
+
+
+# @app.get("/calendars/{calendar_id}/", response_model=CalendarCreate)
+# async def read_calendar(calendar_id: int):
+#     try:
+#         query = select([calendars]).where(calendars.c.id == calendar_id)
+#         calendar = await database.fetch_one(query)
+#         if calendar is None:
+#             raise HTTPException(status_code=404, detail="Calendar not found")
+#         return calendar
+#     except Exception as e:
+#         logging.exception("An error occurred while creating calendar: %s", e)
+#         raise
+
+# # Update an existing calendar
+
+
+# @app.put("/calendars/{calendar_id}/", response_model=CalendarCreate)
+# async def update_calendar(calendar_id: int, calendar: CalendarUpdate):
+#     query = (
+#         calendars.update()
+#         .where(calendars.c.id == calendar_id)
+#         .values(
+#             name=calendar.name,
+#             calendar_type=calendar.calendar_type,
+#             api_key=calendar.api_key,
+#             show_busy_only=calendar.show_busy_only,
+#             is_private=calendar.is_private,
+#         )
+#     )
+#     await database.execute(query)
+#     return {**calendar.dict(), "id": calendar_id}
+
+# # Delete a calendar by its ID
+
+
+# @app.delete("/calendars/{calendar_id}/")
+# async def delete_calendar(calendar_id: int):
+#     query = calendars.delete().where(calendars.c.id == calendar_id)
+#     await database.execute(query)
+#     return {"message": "Calendar deleted successfully"}
+
+
+# @app.get("/")
+# async def read_root():
+#     return {"message": "Hello, FastAPI!"}
+
+# # Configure logging to write to a file
+# logging.basicConfig(
+#     filename='app.log',  # Specify the log file name
+#     level=logging.DEBUG,  # Set the logging level
+#     # Set the log message format
+#     format='%(asctime)s - %(levelname)s - %(message)s'
+# )
+
+
+# @app.on_event("startup")
+# async def startup():
+#     try:
+#         await database.connect()
+#     except Exception as e:
+#         logging.error("Failed to connect to the database: %s", e)
+
+
+# @app.on_event("shutdown")
+# async def shutdown():
+#     await database.disconnect()
+
+class TimeRequest(BaseModel):
+    time_date: str
+
+
+@app.post("/store-check-time")
+def store_check_time(time_req: TimeRequest):
+    time_str = time_req.time_date
     try:
-        query = calendars.insert().values(
-            name=calendar.name,
-            calendar_type=calendar.calendar_type,
-            api_key=calendar.api_key,
-            show_busy_only=calendar.show_busy_only,
-            is_private=calendar.is_private,
-        )
-        last_record_id = await database.execute(query)
-        return {**calendar.dict(), "id": last_record_id}
-    except Exception as e:
-        logging.exception("An error occurred while creating calendar: %s", e)
-        raise
-
-# Read a specific calendar by its ID
-
-
-@app.get("/calendars/{calendar_id}/", response_model=CalendarCreate)
-async def read_calendar(calendar_id: int):
-    try:
-        query = select([calendars]).where(calendars.c.id == calendar_id)
-        calendar = await database.fetch_one(query)
-        if calendar is None:
-            raise HTTPException(status_code=404, detail="Calendar not found")
-        return calendar
-    except Exception as e:
-        logging.exception("An error occurred while creating calendar: %s", e)
-        raise
-
-# Update an existing calendar
-
-
-@app.put("/calendars/{calendar_id}/", response_model=CalendarCreate)
-async def update_calendar(calendar_id: int, calendar: CalendarUpdate):
-    query = (
-        calendars.update()
-        .where(calendars.c.id == calendar_id)
-        .values(
-            name=calendar.name,
-            calendar_type=calendar.calendar_type,
-            api_key=calendar.api_key,
-            show_busy_only=calendar.show_busy_only,
-            is_private=calendar.is_private,
-        )
-    )
-    await database.execute(query)
-    return {**calendar.dict(), "id": calendar_id}
-
-# Delete a calendar by its ID
-
-
-@app.delete("/calendars/{calendar_id}/")
-async def delete_calendar(calendar_id: int):
-    query = calendars.delete().where(calendars.c.id == calendar_id)
-    await database.execute(query)
-    return {"message": "Calendar deleted successfully"}
-
-
-@app.get("/")
-async def read_root():
-    return {"message": "Hello, FastAPI!"}
-
-# Configure logging to write to a file
-logging.basicConfig(
-    filename='app.log',  # Specify the log file name
-    level=logging.DEBUG,  # Set the logging level
-    # Set the log message format
-    format='%(asctime)s - %(levelname)s - %(message)s'
-)
-
-
-@app.on_event("startup")
-async def startup():
-    try:
-        await database.connect()
-    except Exception as e:
-        logging.error("Failed to connect to the database: %s", e)
-
-
-@app.on_event("shutdown")
-async def shutdown():
-    await database.disconnect()
+        time_obj = datetime.fromisoformat(time_str)
+        time_obj_utc = time_obj.astimezone(timezone.utc)
+        db = SessionLocal()
+        existing_time = db.query(calendar.Times).filter(
+            calendar.Times.time_date == time_obj_utc).first()
+        if existing_time:
+            return {"message": "Time already exists in the database"}
+        else:
+            db.add(calendar.Times(time_date=time_obj_utc))
+            db.commit()
+            return {"message": "Time inserted successfully"}
+    except ValueError:
+        raise HTTPException(status_code=400, detail="Invalid datetime format")
